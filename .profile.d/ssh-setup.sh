36026638ef7ac43070e7e9f0df0fcd9b568f2865
Matthew Macdonald-Wallace <theprofessor@threedrunkensysadsonthe.net>
2016-10-24 08:37:20 +0100

    Initial SSH tunnel script

---
+++ .profile.d/ssh-setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+echo $0: creating public and private key files
+
+# Create the .ssh directory
+mkdir -p ${HOME}/.ssh
+chmod 700 ${HOME}/.ssh
+
+# Create the public and private key files from the environment variables.
+echo "${DEPLOYMENT_PUBLIC_KEY}" > ${HOME}/.ssh/id_rsa.pub
+chmod 644 ${HOME}/.ssh/id_rsa.pub
+
+# Note use of double quotes, required to preserve newlines
+echo "${DEPLOYMENT_PRIVATE_KEY}" > ${HOME}/.ssh/id_rsa
+chmod 600 ${HOME}/.ssh/id_rsa
+
+# Preload the known_hosts file  (see "version 2" below)
+
+# Start the SSH tunnel if not already running
+SSH_CMD="ssh -f -i ${HOME}/.ssh/id_rsa -N -L 2222:${www01.agileoperations.co.uk}:22 ${REMOTE_USER}@${REMOTE_SITE}"
+
+PID=`pgrep -f "${SSH_CMD}"`
+if [ $PID ] ; then
+    echo $0: tunnel already running on ${PID}
+else
+    echo $0 launching tunnel
+    $SSH_CMD
+fi
